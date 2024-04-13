933485ed4be51a5aac674ad5ae909de61fec38d3
ckury <155270699+ckury@users.noreply.github.com>
2024-04-13 11:10:43 -0400

    Commit

--- startup-script.sh
+++ startup-script.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-name=$(uname -n)
+machinename=$(uname -n)
 
-gcloud logging write production_log "${name}: New VM Created"
+gcloud logging write production_log "${machinename}: New VM Created"
 
 cd /
 mkdir sentiment-data
@@ -10,14 +10,14 @@ mkdir sentiment-data
 cd sentiment-data/
 
 apt install -y python3-pip
-gcloud logging write production_log "${name}: Installed pip"
+gcloud logging write production_log "${machinename}: Installed pip"
 
 curl -H 'Cache-Control: no-cache, no-store' -o requirements.txt https://raw.githubusercontent.com/ckury/uconn-sentiment-automation/main/requirements.txt
-gcloud logging write production_log "${name}: Downloaded requirements.txt"
+gcloud logging write production_log "${machinename}: Downloaded requirements.txt"
 
 python3 -m pip install -r requirements.txt --break-system-packages
 
 curl -H 'Cache-Control: no-cache, no-store' -o taskmanager.py https://raw.githubusercontent.com/ckury/uconn-sentiment-automation/main/taskmanager.py
-gcloud logging write production_log "${name}: Downloaded taskmanager"
+gcloud logging write production_log "${machinename}: Downloaded taskmanager"
 
-gcloud logging write production_log "${name}: taskmanager: $(python3 taskmanager.py get-task)"
+gcloud logging write production_log "${machinename}: taskmanager: $(python3 taskmanager.py get-task)"
