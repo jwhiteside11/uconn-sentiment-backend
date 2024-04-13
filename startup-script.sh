b4016ca9e481c4b0956f0567ecb8c3f4de380e86
ckury <155270699+ckury@users.noreply.github.com>
2024-04-13 11:01:18 -0400

    Commit

--- startup-script.sh
+++ startup-script.sh
@@ -9,7 +9,15 @@ mkdir sentiment-data
 
 cd sentiment-data/
 
+apt install -y python3-pip
+gcloud logging write production_log "${name}: Installed pip"
+
+curl -H 'Cache-Control: no-cache, no-store' -o requirements.txt https://raw.githubusercontent.com/ckury/uconn-sentiment-automation/main/requirements.txt
+gcloud logging write production_log "${name}: Downloaded requirements.txt"
+
+python3 -m pip install -r requirements.txt --break-system-packages
+
 curl -H 'Cache-Control: no-cache, no-store' -o taskmanager.py https://raw.githubusercontent.com/ckury/uconn-sentiment-automation/main/taskmanager.py
 gcloud logging write production_log "${name}: Downloaded taskmanager"
 
-gcloud logging write production_log "${name}: taskmanager:" + $(python3 taskquerier.py get-task)
+gcloud logging write production_log "${name}: taskmanager: "$(python3 taskmanager.py get-task)
