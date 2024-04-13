7149c4aab95f06bdc05d80d20f07c78a56c000a3
ckury <155270699+ckury@users.noreply.github.com>
2024-04-13 10:32:05 -0400

    Added message when taskmanager is downloaded

--- startup-script.sh
+++ startup-script.sh
@@ -10,5 +10,6 @@ mkdir sentiment-data
 cd sentiment-data/
 
 curl -H 'Cache-Control: no-cache, no-store' -o taskmanager.py https://raw.githubusercontent.com/ckury/uconn-sentiment-automation/main/taskmanager.py
+gcloud logging write production_log "${name}: Downloaded taskmanager"
 
 gcloud logging write production_log "${name}: taskmanager:" + $(python3 taskquerier.py get-task)
