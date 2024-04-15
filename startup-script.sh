93635da44dbb889b219f2b6fbac78cee78bfc1d7
ckury <155270699+ckury@users.noreply.github.com>
2024-04-14 23:42:22 -0400

    Added prototype python function calls for ease of development

--- startup-script.sh
+++ startup-script.sh
@@ -28,8 +28,23 @@ gcloud storage cp gs://production_upload_data_sentiment-analysis-379200/Raw_CC/$
 gcloud logging write production_log "${machinename}: Downloaded inputs ${input_file}"
 
 keywordlocations=$(python3 taskmanager.py task-keywordlocations)
+ticker=$(python3 taskmanager.py task-ticker)
+datetime=$(python3 taskmanager.py task-datetime)
 
 curl -H 'Cache-Control: no-cache, no-store' -o keywordcollector.py https://raw.githubusercontent.com/ckury/uconn-sentiment-automation/main/keywordcollector.py
 gcloud logging write production_log "${machinename}: Downloaded keywordcollector"
 
 gcloud logging write production_log "${machinename}: Downloaded keywords as csv file $(python3 keywordcollector.py ${keywordlocations} keywords.csv)"
+
+# PROTOTYPE Below
+
+python3 scoring.py input_files/ keywords.csv scored_files/
+
+python3 weighting.py scored_files/ keywords.csv weighted_files/
+
+python3 summarizing.py scored_files/ scored_summarized_files/
+
+python3 summarizing.py weighted_files/ weighted_summarized_files/
+
+python3 uploading.py scored_files/ weighted_files/ Detailed ${keywordlocations} ${ticker} ${datetime}
+python3 uploading.py scored_summarized_files/ weighted_summarized_files/ Summary ${keywordlocations} ${ticker} ${datetime}
